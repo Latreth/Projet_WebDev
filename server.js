8f80da0eed088854139e0ec5e7d3da455b42dae7
Latreth <flogoal@wanadoo.fr>
2019-03-24 02:10:21 +0100

    Serveur

---
+++ server.js
@@ -0,0 +1,72 @@
+var websocket = require('ws'),
+	ent = require('ent'); // Permet de bloquer les caractères HTML (sécurité équivalente à htmlentities en PHP)
+
+var ws = new websocket.Server({port: 80});
+console.log("RUNNING ON PORT 80");
+var client = {};
+ws.on('connection', (socket, req) => {
+	let id = Random.Char(16);
+	client[id] = {};
+	console.log(id);
+	let _client = client[id];
+	_client.socket = socket;
+	_client.ip = req.connection.remoteAddress;
+	console.log('CONNECTION FROM ' + _client.ip.substring(7) + " AS " + id);
+	client[id].socket.send(JSON.stringify({msg: JSON.stringify({type: "_id", clientID: id})}));
+	socket.on('message', (data) => {
+		try{
+			console.log(data);
+			data = JSON.parse(data);
+			let clientID = data.cmd.split(' ')[0];
+			let cmd = JSON.parse(data.cmd.substring(clientID.length).trim());
+			let type = cmd.type;
+			if(type == "chat_message"){
+				log(clientID, JSON.stringify({type: "chat_message", pseudo: cmd.pseudo, message: cmd.message}));
+			}
+			if(type == "action"){
+				log(clientID, JSON.stringify({type: "action", pseudo: cmd.pseudo, message: cmd.message}));
+			}
+			if(type == "nouveau_joueur"){
+				log(clientID, JSON.stringify({type: "nouveau_joueur", pseudo: cmd.pseudo}));
+			}
+		}
+		catch(e){
+			console.log(e);
+			return socket.close();
+		}
+	});
+	socket.on('close', () => {
+		delete client[id];
+	});
+});
+var Random = {
+	Char: function(length){
+		if (length === undefined) length = 1;
+		let str = "";
+		let letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9'];
+		for(let i = 0; i < length; ++i) str += letters[this.Int(0, letters.length - 1)];
+		return str
+	},
+	Int: function(min, max){
+		if(min > max) throw "Error : min is more than max";
+		return Math.floor(Math.random()*(max - min + 1)) + min;
+	},
+};
+function log(id, str){
+	let pack = {};
+	pack.msg = str;
+	let data = JSON.stringify(pack);
+	return broadcast(id, data);
+}
+function broadcast(id, msg){
+	if(id == "all"){
+		for(let i in client){
+			let _socket = client[i].socket;
+			_socket.send(msg);
+		}
+	}
+	else{
+		let _socket = client[id].socket;
+		_socket.send(msg);
+	}
+}
---
+++ version_22-03/client.js
@@ -0,0 +1,198 @@
+var grid = initGrille();
+
+function initGrille(){
+	function initLigne(k, p){
+		if(k == 1) return [
+			{type: 'pion', player: p},
+			{type: 'pion', player: p},
+			{type: 'pion', player: p},
+			{type: 'pion', player: p},
+			{type: 'pion', player: p},
+			{type: 'pion', player: p},
+			{type: 'pion', player: p},
+			{type: 'pion', player: p}
+		];
+		if(k == 2) return [
+			{type: 'tour', player: p},
+			{type: 'fou', player: p},
+			{type: 'cheval', player: p},
+			{type: 'reine', player: p},
+			{type: 'roi', player: p},
+			{type: 'cheval', player: p},
+			{type: 'fou', player: p},
+			{type: 'tour', player: p}
+		];
+	}
+
+	let L = [];
+	let i = 1;
+	while(i <= 8){
+		L[i-1] = false;
+		++i;
+	}
+	let G = [];
+	i = 1;
+	while(i <= 8){
+		G[i-1] = L;
+		++i;
+	}
+	G[0] = initLigne(2, 1);
+	G[1] = initLigne(1, 1);
+	G[6] = initLigne(1, 2);
+	G[7] = initLigne(2, 2);
+	G[0][3].type = 'roi';
+	G[0][4].type = 'reine';
+	return G;
+}
+
+function select(x, y){
+
+	let G = grid;
+	if(!G[x][y]) return;
+	let type = G[x][y].type;
+	let p = G[x][y].player;
+	let moves = [];
+
+	if(type == 'pion'){
+		if(p == 1){
+			if(!G[x+1][y]) moves.push('+0+1');
+			if(x == 1 && !G[x+2][y]) moves.push('+0+2');
+		}
+		else{
+			if(!G[x-1][y]) moves.push('+0-1');
+			if(x == 6 && !G[x-2][y]) moves.push('+0-2');
+		}
+		if(!!G[x+1] && !!G[x+1][y+1] && G[x+1][y+1].player != p) moves.push('+1+1');
+		if(!!G[x-1] && !!G[x-1][y+1] && G[x-1][y+1].player != p) moves.push('-1+1');
+		if(!!G[x+1] && !!G[x+1][y-1] && G[x+1][y-1].player != p) moves.push('+1-1');
+		if(!!G[x-1] && !!G[x-1][y-1] && G[x-1][y-1].player != p) moves.push('-1-1');
+	}
+
+	if(type == 'tour' || type == "reine"){
+		for(let i = 1; i <= 8; ++i){
+			if(!G[x][y+i]) moves.push('+' + i + '+0');
+			if(!!G[x][y+i] && G[x][y+i].player == p){
+				break;
+			}
+			if(!!G[x][y+i] && G[x][y+i].player != p){
+				moves.push('+' + i + '+0');
+				break;
+			}
+		}
+		for(let i = 1; i <= 8; ++i){
+			if(!G[x][y-i]) moves.push('-' + i + '+0');
+			if(!!G[x][y-i] && G[x][y-i].player == p){
+				break;
+			}
+			if(!!G[x][y-i] && G[x][y-i].player != p){
+				moves.push('-' + i + '+0');
+				break;
+			}
+		}
+		for(let i = 1; i <= 8; ++i){
+			if(!!G[x+i] && !G[x+i][y]) moves.push('+0+' + i);
+			if(!!G[x+i] && !!G[x+i][y] && G[x+i][y].player == p){
+				break;
+			}
+			if(!!G[x+i] && !!G[x+i][y] && G[x+i][y].player != p){
+				moves.push('+0+' + i);
+				break;
+			}
+		}
+		for(let i = 1; i <= 8; ++i){
+			if(!!G[x-i] && !G[x-i][y]) moves.push('+0+' + i);
+			if(!!G[x-i] && !!G[x-i][y] && G[x-i][y].player == p){
+				break;
+			}
+			if(!!G[x-i] && !!G[x-i][y] && G[x-i][y].player != p){
+				moves.push('+0-' + i);
+				break;
+			}
+		}
+	}
+
+	if(type == 'fou' || type == "reine"){
+		for(let i = 1; i <= 8; ++i){
+			if(!!G[x+i] && !G[x+i][y+i]) moves.push('+' + i + '+' + i);
+			if(!!G[x+i] && !!G[x+i][y+i] && G[x+i][y+i].player == p){
+				break;
+			}
+			if(!!G[x+i] && !!G[x+i][y+i] && G[x+i][y+i].player != p){
+				moves.push('+' + i + '+' + i);
+				break;
+			}
+		}
+		for(let i = 1; i <= 8; ++i){
+			if(!!G[x-i] && !G[x-i][y+i]) moves.push('-' + i + '+' + i);
+			if(!!G[x-i] && !!G[x-i][y+i] && G[x-i][y+i].player == p){
+				break;
+			}
+			if(!!G[x-i] && !!G[x-i][y+i] && G[x-i][y+i].player != p){
+				moves.push('-' + i + '+' + i);
+				break;
+			}
+		}
+		for(let i = 1; i <= 8; ++i){
+			if(!!G[x+i] && !G[x+i][y-i]) moves.push('+' + i + '-' + i);
+			if(!!G[x+i] && !!G[x+i][y-i] && G[x+i][y-i].player == p){
+				break;
+			}
+			if(!!G[x+i] && !!G[x+i][y-i] && G[x+i][y-i].player != p){
+				moves.push('+' + i + '-' + i);
+				break;
+			}
+		}
+		for(let i = 1; i <= 8; ++i){
+			if(!!G[x-i] && !G[x-i][y-i]) moves.push('-' + i + '-' + i);
+			if(!!G[x-i] && !!G[x-i][y-i] && G[x-i][y-i].player == p){
+				break;
+			}
+			if(!!G[x-i] && !!G[x-i][y-i] && G[x-i][y-i].player != p){
+				moves.push('-' + i + '-' + i);
+				break;
+			}
+		}
+	}
+
+	if(type == 'cheval'){
+		if(!!G[x+1] && (!G[x+1][y+2] || !!G[x+1][y+2] && G[x+1][y+2].player != p)) moves.push('+1+2');
+		if(!!G[x-1] && (!G[x-1][y+2] || !!G[x-1][y+2] && G[x-1][y+2].player != p)) moves.push('-1+2');
+		if(!!G[x+1] && (!G[x+1][y-2] || !!G[x+1][y-2] && G[x+1][y-2].player != p)) moves.push('+1-2');
+		if(!!G[x-1] && (!G[x-1][y-2] || !!G[x-1][y-2] && G[x-1][y-2].player != p)) moves.push('-1-2');
+		if(!!G[x+2] && (!G[x+2][y+1] || !!G[x+2][y+1] && G[x+2][y+1].player != p)) moves.push('+2+1');
+		if(!!G[x-2] && (!G[x-2][y+1] || !!G[x-2][y+1] && G[x-2][y+1].player != p)) moves.push('-2+1');
+		if(!!G[x+2] && (!G[x+2][y-1] || !!G[x+2][y-1] && G[x+2][y-1].player != p)) moves.push('+2-1');
+		if(!!G[x-2] && (!G[x-2][y-1] || !!G[x-2][y-1] && G[x-2][y-1].player != p)) moves.push('-2-1');
+	}
+
+	if(type == "roi"){
+		if(!!G[x+1] && (!G[x+1][y] || !!G[x+1][y] && G[x+1][y].player != p)) moves.push('+1+0');
+		if(!!G[x-1] && (!G[x-1][y] || !!G[x-1][y] && G[x-1][y].player != p)) moves.push('-1+0');
+		if(!G[x][y+1] || !!G[x][y+1] && G[x][y+1].player != p) moves.push('+0+1');
+		if(!G[x][y-1] || !!G[x][y-1] && G[x][y-1].player != p) moves.push('+0-1');
+		if(!!G[x+1] && (!G[x+1][y+1] || !!G[x+1][y+1] && G[x+1][y+1].player != p)) moves.push('+1+1');
+		if(!!G[x-1] && (!G[x-1][y+1] || !!G[x-1][y+1] && G[x-1][y+1].player != p)) moves.push('-1+1');
+		if(!!G[x+1] && (!G[x+1][y-1] || !!G[x+1][y-1] && G[x+1][y-1].player != p)) moves.push('+1-1');
+		if(!!G[x-1] && (!G[x-1][y-1] || !!G[x-1][y-1] && G[x-1][y-1].player != p)) moves.push('-1-1');
+	}
+
+	return restrict(moves, x, y);
+}
+
+function restrict(L, y, x){
+	let D = [];
+	L.forEach((d) => {
+		let dx = parseInt(d[3]);
+		if(d[2] == '-') dx = -dx;
+		let dy = parseInt(d[1]);
+		if(d[0] == '-') dy = -dy;
+		if(x+dx >= 0 && x+dx < 8 && y+dy >= 0 && y+dy < 8) D.push([x, y, dx, dy]);
+	});
+	return D;
+}
+
+function move(x, y, dx, dy){
+	G[x+dx][y+dy] = G[x][y];
+	G[x][y] = false;
+	deplacement(y, x, dy, dx);
+}
